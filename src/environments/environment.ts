53598d0d885b6d1668121a9dce22027df61b9375
luckLuis <jacomeluis7@gmail.com>
2022-06-10 06:14:09 -0500

    upload-image-firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  
+  firebaseConfig : {
+    apiKey: "AIzaSyA2F9QEVG7BOzeGQRgOBecDt3WObM0YVtI",
+    authDomain: "proyectos-2022a-cdc6c.firebaseapp.com",
+    projectId: "proyectos-2022a-cdc6c",
+    storageBucket: "proyectos-2022a-cdc6c.appspot.com",
+    messagingSenderId: "347734439295",
+    appId: "1:347734439295:web:ccabf4bb33c48c814b964a",
+    measurementId: "G-2H08M9Y5K1"
+  }
 };
 
 /*
